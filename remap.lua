fa8779de47a8a0e21183cfae94d2ac9fb8650fce
Edouard <128392994+WarreDevriese@users.noreply.github.com>
2024-02-18 19:48:51 +0100

    first commit

---
+++ remap.lua
@@ -0,0 +1,42 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- move highlighted text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- append line below to current line but keep cursor in the same place
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep cursor in middle with C-u and C-d
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in middle cycling through searches with n and N
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste over highlighted text without losing paste buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete to void register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- format according to language conventions, experiment for now
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quickfix navigation, read up on this concept!
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- search and replace all occurences of the current word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
